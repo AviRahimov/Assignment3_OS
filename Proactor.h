3c7546875dc03ab2ca143ae206583328fa6de143
yonatanb <jonatan250199@gmail.com>
2024-03-01 12:09:18 +0200

    Added proactor

--- Proactor.h
+++ Proactor.h
@@ -1,7 +1,44 @@
-// proactor.h
-#ifndef PROACTOR_H
-#define PROACTOR_H
+# define PROACTOR_H
+# ifdef PROACTOR_H
 
-void start_proactor(int port);
+typedef struct Proactor
+{
+    // the curr proactor
+    Proactor_node *head;
+    // indicate if the proactor is running
+    bool is_running;
+    // thread
+    pthread_t thread;
+    // size of the proactor
+    int size;
+} Proactor;
 
-#endif
+typedef struct Proactor_node
+{
+    // the socket
+    int socket;
+    // handler
+    handler_t handler;
+    // the next proactor
+    Proactor_node *next;
+} Proactor_node;
+
+// this method is respinsible for creating a proactor, a list of sockets and their appropriate handlers
+Proactor *create_proactor(int size);
+
+// this function start the proactor work
+int runProactor(Proactor * proactor);
+
+// this function stop the proactor work
+int stopProactor(Proactor * proactor);
+
+// this function add a new socket fd and its handler to the proactor
+int addSocket(Proactor * proactor, int socket, handler_t handler);
+
+// this function remove a socket from the proactor
+int removeSocket(Proactor * proactor, int socket);
+
+// destroy the proactor
+int destroyProactor(Proactor * proactor);
+
+# endif // PROACTOR_H
